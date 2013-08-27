8e56cead967f0d27336b9b7ec9bf8e6e5b402b59
Daniel Escobar <danielf.5@hotmail.com>
2013-08-27 16:29:45 -0500

    commit subiendo ProyectoTarea

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoTarea::Application.config.secret_token = 'eeeaef95d01884f00027e23e91625e0882aa61042da188130cb0c895026e9f9b11c019fd0e8da0d9fea7fed7ea73820f85caba6bac75f60b2db9cfdf2d5f1ce5'
