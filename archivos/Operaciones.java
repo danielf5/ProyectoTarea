1a5965db95a38774e9b156f0736d82489dac8af5
Daniel Escobar <danielf.5@hotmail.com>
2013-08-27 18:58:27 -0500

    segundo commit con suma

---
+++ archivos/Operaciones.java
@@ -0,0 +1,18 @@
+public class Operaciones
+{
+	public static void main (String args[])
+	{
+		new Operaciones();
+	}
+	public Operaciones()
+	{
+		this.Suma();
+	}
+	private Suma()
+	{
+		int a = 2;
+		int b = 5;
+		int c = a+b;
+		System.out.println(c);
+	}
+}
